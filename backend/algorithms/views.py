e3dfc34e4d6f42e3fe60d9acd0f246415638c3dd
Himanshu Bansal <himanshu.bansal@kelltontech.com>
2019-03-06 16:48:32 +0100

    Basic Structure

--- backend/algorithms/views.py
+++ backend/algorithms/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+import json
+import string
+import random
+import requests
+import re
+import xmltodict
+from haversine import haversine
 
-# Create your views here.
+class PostText(APIView):
+    def post(self, req):
+        print(json.loads(req.body))
+        #Get stats
+        return Response(data={"stats": "Stats Detail"}, status=status.HTTP_200_OK)
+
+class SessionStart(APIView):
+    def post(self, req):
+        print(json.loads(req.body))
+        return Response(data={"words": ["Learning words"]}, status=status.HTTP_200_OK)
+
+class GetActivity(APIView):
+    def post(self, req):
+        print(json.loads(req.body))
+        return Response(data={"activity": "activity"}, status=status.HTTP_200_OK)
+
+class PostActivity(APIView):
+    def post(self, req):
+        print(json.loads(req.body))
+        return Response(data={"isCorrect": "response to activity"}, status=status.HTTP_200_OK)
+
+
+
