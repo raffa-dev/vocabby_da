061ab36eeb4a59a6dea6369e146da1e869f3e18d
Haemanth S P <haemanthsp@gmail.com>
2019-03-06 17:02:42 +0100

    Add dummy api response

--- backend/algorithms/views.py
+++ backend/algorithms/views.py
@@ -21,12 +21,43 @@ class PostText(APIView):
 class SessionStart(APIView):
     def post(self, req):
         print(json.loads(req.body))
-        return Response(data={"words": ["Learning words"]}, status=status.HTTP_200_OK)
+        return Response(data={
+		"words": ['dragon',
+ 			  'appalling',
+ 			  'gentle',
+ 		       	  'fear',
+			  'square',
+			  'spread',
+			  'week',
+			  'flaming',
+			  'autumn',
+			  'volunteer',
+			  'diametrically',
+			  'desert',
+			  'home',
+			  'penetrate',
+			  'copy',
+			  'sapphire',
+			  'child',
+			  'time',
+			  '_',
+ 			  'cycle']},
+	      status=status.HTTP_200_OK)
 
 class GetActivity(APIView):
     def post(self, req):
         print(json.loads(req.body))
-        return Response(data={"activity": "activity"}, status=status.HTTP_200_OK)
+        return Response(data={
+            "activity": {
+                'sentences': ['but our readers already know that we may identify the\nterrible celestial ______ with our gentle friend the moon, who would not\nbe greatly flattered by the comparison.\n\n',
+              'these constitute the ______.\n\n',
+                'in order\nto frighten off the black ______, the natives fired shots at the\nhalf-devoured orb, accompanying their volley with the most appalling\nyells.'],
+                'options': ['ebook',
+                            'translator',
+                            'astronomy',
+                            'dragon'],
+                'activityType': 0,
+                'activityId': 6671}}, status=status.HTTP_200_OK)
 
 class PostActivity(APIView):
     def post(self, req):
