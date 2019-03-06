188b009839406e4d7a3ef9a3b9d6585075dfbf70
Haemanth S P <haemanthsp@gmail.com>
2019-03-06 20:03:58 +0100

    Handles session

--- backend/algo/learner.py
+++ backend/algo/learner.py
@@ -1,7 +1,8 @@
 """Module to model the user learning and logic around it."""
 
+import pickle
+import random
 import numpy as np
-# from activity import Session
 
 
 class Learner(object):
@@ -11,6 +12,16 @@ class Learner(object):
 
     def add_book(self, book):
         self.tutors.append(Tutor(self, book))
+
+    @staticmethod
+    def load(learner_id):
+        with open(learner_id + '.p', 'rb') as learner_file:
+            learner = pickle.load(learner_file)
+        return learner
+
+    def save(self):
+        with open(self.name + '.p', 'wb') as learner_file:
+            pickle.dump(self, learner_file)
 
 
 class Tutor(object):
@@ -25,7 +36,7 @@ class Tutor(object):
         self.sessions.append(Session(self, critical_nodes))
 
     def update(self, token, response):
-        pass
+        sign = 1 if response else -1
 
     def get_critical_nodes(self):
         # TODO: Update with proper implementation
@@ -33,33 +44,59 @@ class Tutor(object):
         n_choice = np.random.choice(len(roots), 20)
         return [self.book.roots[roots[i]] for i in n_choice]
 
-    def load(self):
-        pass
-
-    def save(self):
-        pass
-
 
 class Session(object):
     def __init__(self, tutor, tokens):
         self.tutor = tutor
         self.book = self.tutor.book
         self.graph = self.book.net.vocab_net
-        self.tokens = tokens
+        self.tokens = {t.word: t for t in tokens}
+        self.queue = list(self.tokens.keys())
+        self.answers = {}
 
-    def create_activity(self, root, activity_type):
-        word = np.random.choice(root.children)
-        sentences = list(set(s.text.text.replace(word.text, '______')
+    def _create_activity(self, root, activity_type):
+        """"""
+        if activity_type == 0:
+            word = np.random.choice(root.children)
+            sentences = list(
+                    set(s.text.text.replace(word.text, '______')
                         for s in word.sentences))[:3]
-        distractors = self.get_distractors(root) + [word.text]
-        np.random.shuffle(distractors)
-        return {"sentences": sentences,
-                "options": distractors,
-                "answer": word.text,
-                "ActivityType": activity_type}
+            distractors = self._get_distractors(root) + [word.text]
+            np.random.shuffle(distractors)
+            activity_id = random.randint(1000, 100000)
+            self.answers.update(
+                    {activity_id:
+                        {'index': distractors.index(word.text),
+                         'root': root}})
+            return {"sentences": sentences,
+                    "options": distractors,
+                    "activityType": activity_type,
+                    "activityId": activity_id}
 
-    def get_distractors(self, root):
-        # return [np.random.choice(self.book.roots[w])
-        return [w
+    def _get_distractors(self, root):
+        return [np.random.choice(self.book.roots[w].children).text
                 for w, wt in self.graph[root.word].items()
                 if wt['weight'] < 0.8][:3]
+
+    def _activity_selector(self):
+        return 0
+
+    def next_acitivity(self):
+        if self.queue:
+            word = self.queue[0]
+            activity_type = self._activity_selector()
+            return self._create_activity(
+                 self.tokens[word], activity_type)
+        else:
+            return {'activityType': '-1'}
+
+    def update(self, root, response):
+        word = self.queue.pop(0)
+        if not response:
+            self.queue.append(word)
+        self.tutor.update(root, response)
+
+    def evaluate(self, activity_id, selection):
+        is_correct = self.answers[activity_id]['index'] == selection
+        self.update(self.answers[activity_id]['root'], is_correct)
+        return {'isCorrect': is_correct}
