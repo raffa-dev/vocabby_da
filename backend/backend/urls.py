b51fe338c3383048ad3db053c4b49ff6221579ad
Himanshu Bansal <himanshu.bansal@kelltontech.com>
2019-04-17 20:15:58 +0200

    UI improvement

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     url(r'^api/v1/getwordlist$', views.SessionStart.as_view(), name='get words list'),
     url(r'^api/v1/getactivity$', views.GetActivity.as_view(), name='get activity'),
     url(r'^api/v1/postactivity$', views.PostActivity.as_view(), name='post activity'),
+    url(r'^api/v1/getlevel$', views.GetLevel.as_view(), name='get level'),
 ]
